250f93b34087a314db19130ad7a88cfb16001074
Zach Wright <zwright@umich.edu>
2018-04-18 11:35:25 -0400

    KPMP 31: Basic working example with multiple files

--- src/main/java/org/kpmp/FileUploadController.java
+++ src/main/java/org/kpmp/FileUploadController.java
@@ -1,13 +1,6 @@
 package org.kpmp;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,19 +8,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
 
-    @RequestMapping(value = "/upload_old", method = RequestMethod.POST)
-    public void uploadFile(@RequestBody Map<String, Object> payload, HttpServletRequest request) {
-        System.out.println(payload);
-    }
-
     @PostMapping("/upload")
     public String handleFileUpload(@RequestParam("files") MultipartFile[] uploadfiles) {
-        String responseString = "You uploaded:";
+        String responseString = "Success! You uploaded: ";
         for (MultipartFile file : uploadfiles) {
-            System.out.println(file.getOriginalFilename() + ": " + file.getSize());
-            responseString += file.getOriginalFilename() + ": " + file.getSize() + "<br/>";
+            responseString += file.getOriginalFilename() + " (" + file.getSize() + " bytes) ";
         }
-        return responseString;
+        System.out.println(responseString);
+        return "{\"message\": \"" + responseString + "\"}";
     }
 
 }
