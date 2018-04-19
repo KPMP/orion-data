d9d737b25c49c9b31a39749d7c02e0fadfab34c9
Zach Wright <zwright@umich.edu>
2018-04-19 16:20:52 -0400

    Single file upload

--- src/main/java/org/kpmp/FileUploadController.java
+++ src/main/java/org/kpmp/FileUploadController.java
@@ -8,12 +8,20 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
 
-    @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("files") MultipartFile[] uploadfiles) {
+    @PostMapping("/multiupload")
+    public String handleMultiFileUpload(@RequestParam("files") MultipartFile[] uploadfiles) {
         String responseString = "Success! You uploaded: ";
         for (MultipartFile file : uploadfiles) {
             responseString += file.getOriginalFilename() + " (" + file.getSize() + " bytes) ";
         }
+        System.out.println(responseString);
+        return "{\"message\": \"" + responseString + "\"}";
+    }
+
+    @PostMapping("/upload")
+    public String handleFileUpload(@RequestParam("qqfile") MultipartFile uploadFile) {
+        String responseString = "Success! You uploaded: " +
+                uploadFile.getOriginalFilename() + " (" + uploadFile.getSize() + " bytes) ";
         System.out.println(responseString);
         return "{\"message\": \"" + responseString + "\"}";
     }
