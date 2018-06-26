8991791ab4bfe77cd9f5e99787a243a0c540238d
Becky Reamy <rlreamy@umich.edu>
2018-06-25 21:55:52 -0400

    KPMP-84: Initial code to add documentation for endpoints

---
+++ src/main/java/org/kpmp/view/ViewUploadsControllerDocumentation.java
@@ -0,0 +1,52 @@
+package org.kpmp.view;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kpmp.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class ViewUploadsControllerDocumentation {
+
+	@Rule
+	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@Before
+	public void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+				.apply(documentationConfiguration(this.restDocumentation)).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void viewUploads() throws Exception {
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/viewUploads")).andExpect(status().isOk())
+				.andDo(document("viewUploads"));
+	}
+
+}
