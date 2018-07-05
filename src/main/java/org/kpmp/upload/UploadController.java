65e6de0375872cc08270468a76c0484431724e42
Zach Wright <zwright@umich.edu>
2018-07-05 16:27:27 -0400

    KPMP 142: Invalidate session

--- src/main/java/org/kpmp/upload/UploadController.java
+++ src/main/java/org/kpmp/upload/UploadController.java
@@ -140,6 +140,7 @@ public class UploadController {
 				UploadPackageMetadata uploadPackageMetadata = new UploadPackageMetadata(uploadPackage);
 				metadataHandler.saveUploadPackageMetadata(uploadPackageMetadata, filePath);
 				log.info(saveMetadata.format(new Object[]{uploadPackage.getId()}));
+				session.invalidate();
 			} catch (IOException e) {
 				log.error("Unable to save metadata", e);
 				return "{\"success\": " + false + "}";
