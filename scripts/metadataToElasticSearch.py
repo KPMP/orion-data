c0ec2f68da177b8197b196c1f5cf29fdc10cc773
Becky Reamy <rlreamy@umich.edu>
2018-07-11 10:16:47 -0400

    KPMP-141: Changes requested in code review

--- scripts/metadataToElasticSearch.py
+++ scripts/metadataToElasticSearch.py
@@ -9,22 +9,19 @@ output = open ("/data/combinedMetadata.json", "w")
 for subdir, dirs, files in os.walk(rootdir):
     for potentialFile in files:
         if potentialFile == 'metadata.json':
-            with open(os.path.join(subdir, potentialFile)) as f:
+            with open(os.path.join(subdir, potentialFile), "r") as f:
                 packageIndexHolder = {}
-                packageIndex = {}
                 packageMetadata = {}
                 submitter = {}
                 data = json.load(f)
-                packageIndex["_id"] = data["id"]
-                packageIndex["_index"] = "data_lake"
-                packageIndex["_type"] = "doc"
+                packageIndex = {"_id": data["id"], "_index": "data_lake", "_type": "doc"}
                 packageIndexHolder["index"] = packageIndex
                 packageMetadata["subjectId"] = data["subjectId"]
                 packageMetadata["experimentId"] = data["experimentId"]
                 packageMetadata["experimentDate"] = data["experimentDate"]
                 packageMetadata["createdAt"] = data["createdAt"]
                 packageMetadata["packageType"] = data["packageType"]
-                submitter["firstName"] = data["submitterFirstName"];
+                submitter["firstName"] = data["submitterFirstName"]
                 submitter["lastName"] = data["submitterLastName"]
                 packageMetadata["submitter"] = submitter
                 packageMetadata["institution"] = data["institution"]
@@ -33,24 +30,20 @@ for subdir, dirs, files in os.walk(rootdir):
                 fileMetadatas = []
                 files = []
                 for fileUpload in data['files']:
-                    fileMetadata = {}
+                    fileMetadata = { "_index": "data_lake", "_type": "doc", "_id": fileUpload["universalId"], "routing": data["id"]}
                     fileMetadataHolder = {}
-                    fileMetadata["_index"] = "data_lake"
-                    fileMetadata["_type"] = "doc"
-                    fileMetadata["_id"] = fileUpload["universalId"]
-                    fileMetadata["routing"] = data["id"]
                     fileMetadataHolder["index"] = fileMetadata
-                    file = {}
-                    file["path"] = fileUpload["path"]
-                    file["size"] = fileUpload["size"]
-                    file["fileName"] = fileUpload["fileName"]
-                    file["description"] = fileUpload["description"]
+                    theFile = {}
+                    theFile["path"] = fileUpload["path"]
+                    theFile["size"] = fileUpload["size"]
+                    theFile["fileName"] = fileUpload["fileName"]
+                    theFile["description"] = fileUpload["description"]
                     joinField = {}
-                    joinField["name"] = "file";
+                    joinField["name"] = "file"
                     joinField["parent"] = data["id"]
-                    file["join_field"] = joinField
+                    theFile["join_field"] = joinField
                     fileMetadatas.append(fileMetadataHolder)
-                    files.append(file)
+                    files.append(theFile)
                 
 
             output.write(json.dumps(packageIndexHolder) + "\n")
