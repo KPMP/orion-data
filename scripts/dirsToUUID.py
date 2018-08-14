616b720a5d9a85a200d428248c590632a7063540
Zach Wright <zwright@umich.edu>
2018-08-14 15:38:12 -0400

    KPMP-180: Check for empty package ID

--- scripts/dirsToUUID.py
+++ scripts/dirsToUUID.py
@@ -3,11 +3,12 @@
 import json
 import os
 
-rootdir = "/data"
+rootdir = "/cygdrive/c/data"
 
 if __name__ == '__main__':
     for root, dirs, files in os.walk(rootdir):
         dirName = ''
+        packageId = ''
         for file in files:
             if file == 'metadata.json':
                 with open(os.path.join(root, file), "r") as metadataFile:
@@ -15,5 +16,6 @@ if __name__ == '__main__':
                     packageId = data["id"]
                     dirName = 'package_' + packageId
         newDirPath = rootdir + '/' + dirName;
-        print "renaming " + root + " to " + newDirPath
-        os.rename(root, newDirPath)
+        if packageId != '':
+            print "renaming " + root + " to " + newDirPath
+            os.rename(root, newDirPath)
