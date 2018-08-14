a45d984db69ea1d4f877380ccb40775cbd1cc9a0
Zach Wright <zwright@umich.edu>
2018-08-14 15:27:42 -0400

    KPMP-180: Rename directories

---
+++ scripts/dirsToUUID.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import json
+import os
+
+rootdir = "/cygdrive/c/data"
+
+if __name__ == '__main__':
+    for root, dirs, files in os.walk(rootdir):
+        dirName = ''
+        for file in files:
+            if file == 'metadata.json':
+                with open(os.path.join(root, file), "r") as metadataFile:
+                    data = json.load(metadataFile)
+                    packageId = data["id"]
+                    dirName = 'package_' + packageId
+        newDirPath = rootdir + '/' + dirName;
+        print "renaming " + root + " to " + newDirPath
+        os.rename(root, newDirPath)
