89d44e21914c583e8a498c02a5e1c8b2164fa7dc
Becky Reamy <rlreamy@umich.edu>
2018-08-24 16:28:35 -0400

    KPMP-149: Added mappings for mongo

--- src/main/java/org/kpmp/packages/Package.java
+++ src/main/java/org/kpmp/packages/Package.java
@@ -1,9 +1,11 @@
 package org.kpmp.packages;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "packages")
 public class Package {
@@ -14,6 +16,8 @@ public class Package {
 	private Date createdAt;
 	private String submitter;
 	private String institution;
+	@Field("files")
+	private List<Attachment> attachments;
 
 	public String getPackageId() {
 		return packageId;
@@ -53,6 +57,14 @@ public class Package {
 
 	public void setInstitution(String institution) {
 		this.institution = institution;
+	}
+
+	public List<Attachment> getAttachments() {
+		return attachments;
+	}
+
+	public void setAttachments(List<Attachment> attachments) {
+		this.attachments = attachments;
 	}
 
 }
