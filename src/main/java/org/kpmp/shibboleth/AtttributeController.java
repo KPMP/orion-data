ec787e0996735b03d333468bcec1eb445d7a022d
Becky Reamy <rlreamy@umich.edu>
2018-09-27 11:39:15 -0400

    KPMP-200: That failed...added null guard and logging

--- src/main/java/org/kpmp/shibboleth/AtttributeController.java
+++ src/main/java/org/kpmp/shibboleth/AtttributeController.java
@@ -1,9 +1,12 @@
 package org.kpmp.shibboleth;
 
 import java.io.UnsupportedEncodingException;
+import java.text.MessageFormat;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +14,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AtttributeController {
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	private static final MessageFormat attributesDisplayName = new MessageFormat("Request|{0}|{1}");
+
 	@RequestMapping(value = "/v1/attributes/displayName", method = RequestMethod.GET)
 	public String getDisplayName(HttpServletRequest request) throws UnsupportedEncodingException {
+		log.info(attributesDisplayName.format(new Object[] { "getDisplayName", request.getAttributeNames() }));
+
 		String value = request.getHeader("displayName");
-		String displayName = new String(value.getBytes("ISO-8859-1"), "UTF-8");
-		return displayName;
+		if (value != null) {
+			String displayName = new String(value.getBytes("ISO-8859-1"), "UTF-8");
+			return displayName;
+		}
+		return "";
 	}
 }
