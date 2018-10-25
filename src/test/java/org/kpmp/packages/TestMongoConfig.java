1bda66c3c32e11921498c7e50493a27c44587e3d
Zach Wright <zwright@umich.edu>
2018-10-25 15:17:53 -0400

    KPMP-337: Use Mongo version

--- src/test/java/org/kpmp/packages/TestMongoConfig.java
+++ src/test/java/org/kpmp/packages/TestMongoConfig.java
@@ -1,7 +1,10 @@
 package org.kpmp.packages;
 
+import static de.flapdoodle.embed.mongo.distribution.Version.Main.PRODUCTION;
+
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -13,6 +16,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import com.mongodb.MongoClient;
 
 import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import de.flapdoodle.embed.mongo.distribution.IFeatureAwareVersion;
 
 @Configuration
 @ComponentScan(basePackages = {
@@ -24,11 +28,20 @@ public class TestMongoConfig {
 	private static final String MONGO_DB_URL = "localhost";
 	private static final String MONGO_DB_NAME = "embeded_db";
 
+	@Value("${embedded.mongodb.version:latest}")
+	private String mongoVersion;
+
+	private IFeatureAwareVersion version = PRODUCTION;
+
 	@Bean
 	public MongoTemplate mongoTemplate() throws IOException {
 		EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
 		mongo.setBindIp(MONGO_DB_URL);
+		if (!mongoVersion.equals("latest")) {
+			mongo.setVersion(mongoVersion);
+		}
 		MongoClient mongoClient = mongo.getObject();
+
 		MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
 		return mongoTemplate;
 	}
