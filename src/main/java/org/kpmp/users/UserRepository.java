8ee010770369e51eeac74436d05949bad2764be6
Becky Reamy <rlreamy@umich.edu>
2018-10-31 15:09:07 -0400

    KPMP-195: Fix build error

--- src/main/java/org/kpmp/users/UserRepository.java
+++ src/main/java/org/kpmp/users/UserRepository.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 public interface UserRepository extends MongoRepository<User, String> {
 
-	public <T extends User> T save(User user);
+	@SuppressWarnings("unchecked")
+	public User save(User user);
 
 	public User findByEmail(String email);
 
