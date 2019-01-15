ef81aa553a1f98c7eb6793ea71baf5778123d97b
Becky Reamy <rlreamy@umich.edu>
2019-01-15 10:27:25 -0500

    KPMP-80: Add tests around new error controller

---
+++ src/main/java/org/kpmp/error/FrontEndError.java
@@ -0,0 +1,24 @@
+package org.kpmp.error;
+
+public class FrontEndError {
+
+	private String error;
+	private String stackTrace;
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getStackTrace() {
+		return stackTrace;
+	}
+
+	public void setStackTrace(String stackTrace) {
+		this.stackTrace = stackTrace;
+	}
+
+}
