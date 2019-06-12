1274ce263f64c328ad5a59990808a9e11bf01972
Becky Reamy <rlreamy@umich.edu>
2019-06-12 16:33:45 -0400

    KPMP-792: Added logging to form controller

--- src/main/java/org/kpmp/forms/FormController.java
+++ src/main/java/org/kpmp/forms/FormController.java
@@ -1,7 +1,9 @@
 package org.kpmp.forms;
 
-import java.text.MessageFormat;
+import javax.servlet.http.HttpServletRequest;
 
+import org.kpmp.ApplicationConstants;
+import org.kpmp.JWTHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +17,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class FormController {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	private static final MessageFormat formRequest = new MessageFormat("Request|{0}");
 
 	private FormRepository repository;
+	private JWTHandler jwtHandler;
 
 	@Autowired
-	public FormController(FormRepository repository) {
+	public FormController(FormRepository repository, JWTHandler jwtHandler) {
 		this.repository = repository;
+		this.jwtHandler = jwtHandler;
 	}
 
 	@RequestMapping(value = "/v1/form", method = RequestMethod.GET)
-	public @ResponseBody Form getFormDTD() {
-		log.info(formRequest.format(new Object[] { "getFormDTD" }));
+	public @ResponseBody Form getFormDTD(HttpServletRequest request) {
+		String userId = jwtHandler.getUserIdFromHeader(request);
+		log.info(ApplicationConstants.LOG_MESSAGE_FORMAT, userId, null, request.getRequestURI(),
+				"Request for all forms");
 		return this.repository.findTopByOrderByVersionDesc();
 	}
 
 	@RequestMapping(value = "/v1/form/version/{version}", method = RequestMethod.GET)
-	public @ResponseBody Form getFormDTD(@PathVariable Double version) {
-		log.info(formRequest.format(new Object[] { "getFormDTD" }));
+	public @ResponseBody Form getFormDTD(@PathVariable Double version, HttpServletRequest request) {
+		String userId = jwtHandler.getUserIdFromHeader(request);
+		log.info(ApplicationConstants.LOG_MESSAGE_FORMAT, userId, null, request.getRequestURI(),
+				"Request for form with version: " + version);
 		return this.repository.findByVersion(version).get(0);
 	}
 
--- src/test/java/org/kpmp/forms/FormControllerTest.java
+++ src/test/java/org/kpmp/forms/FormControllerTest.java
@@ -2,46 +2,98 @@ package org.kpmp.forms;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.kpmp.JWTHandler;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
 
 public class FormControllerTest {
 
 	@Mock
 	private FormRepository repository;
+	@Mock
+	private JWTHandler jwtHandler;
+	@SuppressWarnings("rawtypes")
+	@Mock
+	private Appender appender;
+	@Captor
+	private ArgumentCaptor<LoggingEvent> captureLoggingEvent;
 	private FormController controller;
 
+	@SuppressWarnings("unchecked")
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		controller = new FormController(repository);
+		Logger root = (Logger) LoggerFactory.getLogger(FormController.class);
+		root.addAppender(appender);
+		root.setLevel(Level.INFO);
+		controller = new FormController(repository, jwtHandler);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		controller = null;
+		appender = null;
+		captureLoggingEvent = null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testGetFormDTDNoVersion() {
 		Form expectedForm = mock(Form.class);
 		when(repository.findTopByOrderByVersionDesc()).thenReturn(expectedForm);
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		when(jwtHandler.getUserIdFromHeader(request)).thenReturn("userID");
+		when(request.getRequestURI()).thenReturn("/v1/form");
 
-		assertEquals(expectedForm, controller.getFormDTD());
+		Form formDTD = controller.getFormDTD(request);
+
+		assertEquals(expectedForm, formDTD);
+		verify(appender, times(1)).doAppend(captureLoggingEvent.capture());
+		LoggingEvent event = captureLoggingEvent.getAllValues().get(0);
+		assertEquals("USERID: {} | PKGID: {} | URI: {} | MSG: {} ", event.getMessage());
+		assertEquals("USERID: userID | PKGID: null | URI: /v1/form | MSG: Request for all forms ",
+				event.getFormattedMessage());
+		assertEquals(Level.INFO, event.getLevel());
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testGetFormDTDWithVersion() {
 		Form expectedForm = mock(Form.class);
 		when(repository.findByVersion(1.0)).thenReturn(Arrays.asList(expectedForm));
-		assertEquals(expectedForm, controller.getFormDTD(1.0));
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		when(jwtHandler.getUserIdFromHeader(request)).thenReturn("userID");
+		when(request.getRequestURI()).thenReturn("/v1/form/version/1.0");
+
+		Form formDTD = controller.getFormDTD(1.0, request);
+
+		assertEquals(expectedForm, formDTD);
+		verify(appender, times(1)).doAppend(captureLoggingEvent.capture());
+		LoggingEvent event = captureLoggingEvent.getAllValues().get(0);
+		assertEquals("USERID: {} | PKGID: {} | URI: {} | MSG: {} ", event.getMessage());
+		assertEquals(
+				"USERID: userID | PKGID: null | URI: /v1/form/version/1.0 | MSG: Request for form with version: 1.0 ",
+				event.getFormattedMessage());
+		assertEquals(Level.INFO, event.getLevel());
 	}
 
 }
