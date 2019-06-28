3cf63e611984fde7e84e481d3d04ca1a436e042c
Becky Reamy <rlreamy@umich.edu>
2019-06-28 11:19:37 -0400

    KPMP-1105: Restore Shibboleth related classes

---
+++ src/main/java/org/kpmp/shibboleth/ShibbolethAttributeController.java
@@ -0,0 +1,38 @@
+package org.kpmp.shibboleth;
+
+import java.io.UnsupportedEncodingException;
+import java.text.MessageFormat;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.kpmp.users.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ShibbolethAttributeController {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	private static final MessageFormat attributesDisplayName = new MessageFormat("Request|{0}");
+	private UTF8Encoder encoder;
+	private ShibbolethUserService shibbolethUserService;
+
+	@Autowired
+	public ShibbolethAttributeController(UTF8Encoder encoder, ShibbolethUserService shibbolethUserService) {
+		this.encoder = encoder;
+		this.shibbolethUserService = shibbolethUserService;
+	}
+
+	@RequestMapping(value = "/v1/userInformation", method = RequestMethod.GET)
+	public @ResponseBody User getAttributes(HttpServletRequest request) throws UnsupportedEncodingException {
+		log.info(attributesDisplayName.format(new Object[] { "getAttributes" }));
+
+		User user = shibbolethUserService.getUser(request, encoder);
+		return user;
+	}
+}
