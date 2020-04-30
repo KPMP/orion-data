f11d55ff5720868d52677ea7d111b1bdb674fccc
Becky Reamy <rlreamy@umich.edu>
2020-04-30 14:41:28 -0400

    KPMP-1486: Updated mongo with new file infos

--- scripts/processLargeFileUpload/updateMongo.js
+++ scripts/processLargeFileUpload/updateMongo.js
@@ -1,8 +1,25 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const filesystem = require('fs');
+const path = require('path');
+const { v4: uuidGenerator } = require('uuid');
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'dataLake';
+
+const updatePackage = function(packageId, files, db, callback) {
+	
+	var packages = db.collection("packages");
+        packages.findOneAndUpdate({ "_id": packageId }, {$set: { files: files }}, {new: true}, function(err, doc) {
+		if(err) {
+			console.log("Hit err: " + err);
+		} else {
+			console.log("Successfully updated");
+		}
+		callback();
+	});
+}
+
 
 MongoClient.connect(url, { useUnifiedTopology: true, 'forceServerObjectId' : true }, function(err, client) {
 	assert.equal(null, err);
@@ -13,9 +30,21 @@ MongoClient.connect(url, { useUnifiedTopology: true, 'forceServerObjectId' : tru
 	const args = process.argv.slice(2)
 	const packageId = args[0]
 	
-	var packages = db.collection("packages");
-	packages.findOne({ "_id": packageId }, function(err, document) {
-		assert.equal(null, err);
-		console.log(document);
-	}); 
+	const packageDir = "package_" + packageId
+	const directory = path.join("/data/dataLake", packageDir);
+	var files = filesystem.readdirSync(directory);
+	let fileInfos = [];
+ 	files.forEach(file => {
+		const filePath = path.join(directory, file);
+		let stats = filesystem.statSync(filePath);
+		let fileInfo = {};
+		fileInfo.fileName= file;
+		fileInfo.size=stats["size"];
+		fileInfo._id = uuidGenerator();
+		fileInfos.push(fileInfo);
+	});
+	
+	updatePackage(packageId, fileInfos , db, function() {
+		client.close();
+	});
 });
