173c86f910207961827364fd5ef93cb898eb5b60
Becky Reamy <rlreamy@umich.edu>
2020-05-04 11:44:01 -0400

    KPMP-1486: Continue to fix codacy issues

--- scripts/processLargeFileUpload/processLargeFileUpload.sh
+++ scripts/processLargeFileUpload/processLargeFileUpload.sh
@@ -11,22 +11,22 @@ mkdir "${packageDir}"
 
 cd ~/globusconnectpersonal-3.0.4/
 globus login
-taskId=`globus transfer --format unix --jmespath 'task_id' 936381c8-1653-11ea-b94a-0e16720bb42f:/PROD_INBOX/"${packageId}" 86f02d04-8a2c-11ea-b3bb-0ae144191ee3:"${packageDir}" --recursive`
+taskId=$(globus transfer --format unix --jmespath 'task_id' 936381c8-1653-11ea-b94a-0e16720bb42f:/PROD_INBOX/"${packageId}" 86f02d04-8a2c-11ea-b3bb-0ae144191ee3:"${packageDir}" --recursive)
 
 while [[ "$transferStatus" != "SUCCEEDED" && "$transferStatus" != "FAILED" ]]
 do
-  transferStatus=`globus task show --format unix --jmespath 'status' $taskId`
+  transferStatus=$(globus task show --format unix --jmespath 'status' "${taskId}")
 done
 
-if [ $transferStatus == "FAILED" ]
+if [ "$transferStatus" == "FAILED" ]
 then
 	echo "Transfer Failed. Globus task id was: $taskId"
 	exit
 fi
 
-cd $currentDir
+cd "${currentDir}"
 
-node updateMongo.js $packageId
+node updateMongo.js "${packageId}"
 
 result=$?
 
