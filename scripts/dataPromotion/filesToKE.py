e2cd3322a425ec1bda517b1f6b7cdc4726419bc5
Zach Wright <zwright@umich.edu>
2021-12-10 14:01:02 -0500

    KPMP-3234: Empty check

--- scripts/dataPromotion/filesToKE.py
+++ scripts/dataPromotion/filesToKE.py
@@ -52,18 +52,18 @@ for (package_id, file_name, protocol, metadata_type_id, participant_id, release_
         file_size = 0
     else:
         result = packages.find_one({ "_id": package_id, "files.fileName": file_name}, {"files.$":1})
-        if result.count() == 0:
-            print("File not found in packages collection. Looking in files collection . . . ")
+        if result is None:
+            print("File " + file_name + " not found in packages collection. Looking in files collection . . . ")
             result = files.find_one({ "packageId": package_id, "fileName": file_name})
-        if result.count > 0:
+        if result is not None:
             new_file_name = result["files"][0]["_id"] + "_" + file_name
             file_size = result["files"][0]["size"]
             file_id = result["files"][0]["_id"]
         else:
-            print("File " + file_name  + " in package " + + " was not found. Exiting.")
+            print("File " + file_name  + " in package " + package_id + " was not found. Exiting.")
             sys.exit()
 
-    file_exists_sql = "SELECT file_id FROM files WHERE dl_file_id = %s"
+    file_exists_sql = "SELECT file_id FROM file WHERE dl_file_id = %s"
     cursor4.execute(file_exists_sql, (file_id,))
 
     if cursor4.rowcount == 0:
