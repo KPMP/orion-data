e7155dae1995cb7d007edf41de587a7ec854e89f
Alex Barkell <albarkel@umich.edu>
2022-07-29 14:54:01 -0400

    refactor filesToS3 to allow for 3m-omics zipping

--- scripts/dataPromotion/filesToS3.py
+++ scripts/dataPromotion/filesToS3.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import mysql.connector
 from argparse import ArgumentParser
+from package_zipper import packageZipper
 
 load_dotenv()
 
@@ -19,7 +20,7 @@ parser.add_argument("-v", "--release_ver",
                     required=True)
 
 args = parser.parse_args()
-
+ 
 try:
     mydb = mysql.connector.connect(
         host="localhost",
@@ -36,6 +37,59 @@ except:
     print("ssh ubuntu@qa-atlas.kpmp.org -i ~/.ssh/um-kpmp.pem -L 3306:localhost:3306")
     os.sys.exit()
 
+def update_file_size(file_path, file_id):
+    file_size = os.path.getsize(file_path)
+    values = (file_size, file_id)
+    update_sql = "UPDATE file SET file_size = %s WHERE file_id = %s"
+    print(update_sql % values)
+    cursor2.execute(update_sql, values)
+
+def fetch_expression_file_names(package_id, metadata_type_id):
+    query2 = "SELECT file_name FROM file_pending WHERE package_id = %s AND metadata_type_id = %s"
+    cursor2.execute(query2, (package_id, metadata_type_id))
+    expression_file_names = cursor2.fetchone()[0].replace(";", "")
+    return expression_file_names
+
+def copy_local_file_to_s3_destination(file_path, object_name):
+    command_string = 'aws s3 cp "' + file_path + '" s3://' + destination_bucket + '/' + object_name + '"'
+    print(command_string)
+    response = os.system(command_string)
+    if(response != 0):
+        raise Exception("Failed to upload file " + file_path + " onto s3 bucket " + destination_bucket + "/" + object_name)
+    else:
+        print("File copied to S3")
+
+def copy_s3_file_to_s3_destination(source_object, object_name):
+                    command_string = 'aws s3 cp "s3://' + source_object + '" "s3://' + destination_bucket + '/' + object_name + '"'
+                    os.system(command_string)
+
+
+def insert_into_moved_files(file_name):
+                    insert_sql = "INSERT INTO moved_files (file_name) VALUES (%s)"
+                    cursor2.execute(insert_sql, (file_name,))
+                    mydb.commit()
+
+def move_files(file_path, object_name, file_name):
+    if os.path.exists(file_path):
+        try:
+            print("Moving " + object_name)
+            copy_local_file_to_s3_destination(file_path, object_name)
+            update_count = update_count + 1
+            insert_into_moved_files(file_name)
+        except Exception as err:
+            print(err)
+            pass
+    else:
+        source_object = source_bucket + "/package_" + package_id + "/" + original_file_name
+        print("File not found locally. Trying S3: " + source_object)
+        try:
+            copy_s3_file_to_s3_destination(source_object, object_name)
+            update_count = update_count + 1
+        except:
+            print(err)
+            pass
+
+
 query = ("SELECT file_id, package_id, file_name, metadata_type_id FROM file WHERE release_ver = " + args.release_ver + "AND file_name NOT IN (SELECT file_name FROM moved_files)")
 cursor.execute(query)
 update_count = 0
@@ -45,58 +99,47 @@ for (file_id, package_id, file_name, metadata_type_id) in cursor:
     original_file_name = file_name[37:]
     file_path = datalake_package_dir + original_file_name
     expression_file_names = "barcodes.tsv.gz features.tsv.gz matrix.mtx.gz"
+
     if file_name:
         object_name = package_id + "/" + file_name
         print("Looking for: " + file_path)
+
         if file_name.endswith('expression_matrix.zip'):
             if metadata_type_id == 21:
-                query2 = "SELECT file_name FROM file_pending WHERE package_id = %s AND metadata_type_id = %s"
-                cursor2.execute(query2, (package_id, metadata_type_id))
-                expression_file_names = cursor2.fetchone()[0].replace(";", "")
+                expression_file_names = fetch_expression_file_names(package_id, metadata_type_id)
+            
             print("Creating expression matrix zip file for: " + expression_file_names)
             expression_file_names_arr = expression_file_names.split()
+            
             if not os.path.exists(datalake_package_dir + expression_file_names_arr[0]):
                 for expression_file_name in expression_file_names_arr:
                     source_object = source_bucket + "/package_" + package_id + "/" + expression_file_name
                     command_string = "aws s3 cp s3://" + source_object + " " + datalake_package_dir + expression_file_name
                     print(command_string)
                     os.system(command_string)
+          
             command_string = "cd " + datalake_package_dir + " && zip expression_matrix.zip " + expression_file_names
             print(command_string)
             os.system(command_string)
-            file_size = os.path.getsize(file_path)
-            values = (file_size, file_id)
-            update_sql = "UPDATE file SET file_size = %s WHERE file_id = %s"
-            print(update_sql % values)
-            cursor2.execute(update_sql, values)
-        if not os.path.exists(file_path):
-            source_object = source_bucket + "/package_" + package_id + "/" + original_file_name
-            print("File not found locally. Trying S3: " + source_object)
-            try:
-                command_string = 'aws s3 cp "s3://' + source_object + '" "s3://' + destination_bucket + '/' + object_name + '"'
-                os.system(command_string)
-                update_count = update_count + 1
-            except:
-                print(err)
-                pass
-        else:
-            try:
-                print("Moving " + object_name)
-                command_string = 'aws s3 cp "' + file_path + '" s3://' + destination_bucket + '/' + object_name + '"'
-                response = os.system(command_string)
-                if(response != 0):
-                    raise Exception("Failed to upload file " + file_path + " onto s3 bucket " + destination_bucket + "/" + object_name)
-                update_count = update_count + 1
-                insert_sql = "INSERT INTO moved_files (file_name) VALUES (%s)"
-                cursor2.execute(insert_sql, (file_name,))
-                mydb.commit()
-            except Exception as err:
-                print(err)
-                pass
+            update_file_size(file_path, file_id)
+
+        elif 'omics' in file_name.lower(): # might be something better we can key off of here
+            if 'lipidomics' in file_name.lower():
+                omicsType = 'lipidomics'
+            elif 'metabolomics' in file_name.lower():
+                omicsType = 'metabolomics'
+            elif 'n-glycomics' in file_name.lower():
+                omicsType = 'n-glycomics'
+            packageZipper.zip_package_data(package_id+'_'+omicsType, datalake_package_dir)
+            update_file_size(file_path, file_id)
+
+        move_files(file_path, object_name, file_name)
 
     else:
         print("No file name in record.")
     print("\n")
+
+
 mydb.commit()
 mydb.close()
 print(str(update_count) + " files moved")
