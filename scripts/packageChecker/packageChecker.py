064c9f314502b7060d519e7f4b106b2a594c5f08
rlreamy <rlreamy@umich.edu>
2023-02-22 11:13:43 -0500

    KPMP-4069: Added check that the directory contains all expected files

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -20,6 +20,13 @@ class PackageChecker:
         except:
             print("Unable to connect to database")
 
+    def get_expected_files(self, package):
+        expected_file_information = package['files']
+        expected_file_names = []
+        for file_info in expected_file_information:
+            expected_file_names.append(file_info['fileName'])
+        return expected_file_names
+
     def find_empty_packages(self):
         empty_package_list = []
         missing_package_list = []
@@ -32,29 +39,33 @@ class PackageChecker:
                     try:
                         directory = "/data/dataLake/package_";
                         files = os.listdir(directory + package_id)
+                        expected_file_names = self.get_expected_files(package)
+                        actual_file_names = []
                         if len(files) == 0:
                             empty_package_list.append(package_id)
                         else:
                             for file in files:
-                                print(file)
+                                actual_file_names.append(file)
                                 if file == "metadata.json" and len(files) == 1:
                                     empty_package_list.append(package_id)
+                            if (not set(expected_file_names).issubset(set(actual_file_names))) and not all(p == "metadata.json" for p in actual_file_names):
+                                empty_package_list.append(package_id)
                     except:
                         missing_package_list.append(package_id)
 
         if len(empty_package_list) > 0:
-            message = "TESTING: Missing files in packages: " + ', '.join(empty_package_list)
+            message = "Missing files in packages: " + ', '.join(empty_package_list)
 
             requests.post(
                 slack_url,
                 headers={'Content-type': 'application/json', },
                 data='{"text":"' + message+'"}')
         if len(missing_package_list) > 0:
-            message = "TESTING: Missing package directories for packages: " + ', '.join(missing_package_list)
-            # requests.post(
-            #     slack_url,
-            #     headers={'Content-type': 'application/json', },
-            #     data='{"text":"' + message + '"}')
+            message = "Missing package directories for packages: " + ', '.join(missing_package_list)
+            requests.post(
+                slack_url,
+                headers={'Content-type': 'application/json', },
+                data='{"text":"' + message + '"}')
 
 
 
