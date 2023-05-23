59dc7b9816ed05a8669e50b9dfaa78f830c73c81
Dert1129 <nathancreger1111@gmail.com>
2023-05-23 11:26:09 -0400

    trying difference

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -52,7 +52,7 @@ class PackageChecker:
                                     empty_package_list.append(package_id)
                             if (not set(expected_file_names).issubset(set(actual_file_names))) and not all(p == "metadata.json" for p in actual_file_names):
                                 empty_package_list.append(package_id)
-                                missing_files = set(expected_file_names).symmetric_difference(actual_file_names)
+                                missing_files = set(expected_file_names).difference(actual_file_names)
                                 missing_files_list.append(missing_files)
                             
                     except:
