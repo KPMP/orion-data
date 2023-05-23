d512f3e78f990262fe9d494a430f6ca8c732fecb
Dert1129 <nathancreger1111@gmail.com>
2023-05-23 15:08:24 -0400

    moving out of if statement

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -56,12 +56,11 @@ class PackageChecker:
                                 if file == "metadata.json" and len(files) == 1:
                                     empty_package_list.append(package_id)
                                     
-                            if (actual_file_names):
-                                missing_files_list = set(expected_file_names).difference(set(actual_file_names))
-                                data = [
-                                    [package_id, missing_files_list]
-                                ]
-                                writer.writerows(data)
+                            missing_files_list = set(expected_file_names).difference(set(actual_file_names))
+                            data = [
+                                [package_id, missing_files_list]
+                            ]
+                            writer.writerows(data)
                             if (not set(expected_file_names).issubset(set(actual_file_names))) and not all(p == "metadata.json" for p in actual_file_names):
                                 empty_package_list.append(package_id)
                     except:
