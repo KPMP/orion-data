ea43a8e5bebb4f22935c74eafb5ace146d935a27
Dert1129 <nathancreger1111@gmail.com>
2023-05-25 10:47:30 -0400

    move statements

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -58,19 +58,19 @@ class PackageChecker:
                                 if file == "metadata.json" and len(files) == 1:
                                     empty_package_list.append(package_id)
                                     
-                            missing_files_list = set(expected_file_names).difference(set(actual_file_names))        
                             
-                            
-                            if len(missing_files_list) != 0:
-                                    data = [
-                                    [package_id, missing_files_list]
-                                    ]
-                                    writer.writerows(data)
-                            if (not set(expected_file_names).issubset(set(actual_file_names))) and not all(p == "metadata.json" for p in actual_file_names):
-                                empty_package_list.append(package_id)
                     except:
                         missing_package_list.append(package_id)
-                        
+            missing_files_list = set(expected_file_names).difference(set(actual_file_names))        
+                            
+                            
+            if len(missing_files_list) != 0:
+                    data = [
+                    [package_id, missing_files_list]
+                    ]
+                    writer.writerows(data)
+            if (not set(expected_file_names).issubset(set(actual_file_names))) and not all(p == "metadata.json" for p in actual_file_names):
+                empty_package_list.append(package_id)
         f.close()
             
         if len(empty_package_list) > 0:
