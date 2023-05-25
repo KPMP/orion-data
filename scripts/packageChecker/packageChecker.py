888256015dd199b0c7cb0bc9efa2a8b8e79a5672
Dert1129 <nathancreger1111@gmail.com>
2023-05-25 11:21:27 -0400

    uncomment code

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -75,18 +75,16 @@ class PackageChecker:
             
         if len(empty_package_list) > 0:
             message = "Missing files in packages: " + ', '.join(empty_package_list)
-            print(message)
-            # requests.post(
-            #     slack_url,
-            #     headers={'Content-type': 'application/json', },
-            #     data='{"text":"' + message+'"}')
-        # if len(missing_package_list) > 0:
-        #     message = "Missing package directories for packages: " + ', '.join(missing_package_list)
-        #     print(message)
-            # requests.post(
-            #     slack_url,
-            #     headers={'Content-type': 'application/json', },
-            #     data='{"text":"' + message + '"}')
+            requests.post(
+                slack_url,
+                headers={'Content-type': 'application/json', },
+                data='{"text":"' + message+'"}')
+        if len(missing_package_list) > 0:
+            message = "Missing package directories for packages: " + ', '.join(missing_package_list)
+            requests.post(
+                slack_url,
+                headers={'Content-type': 'application/json', },
+                data='{"text":"' + message + '"}')
 
 
 
