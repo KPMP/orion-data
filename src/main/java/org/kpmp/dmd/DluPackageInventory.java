d2d6fb1baa4fed51cee38b3a3395b0f365bbbab3
dert1129 <nathancreger1111@gmail.com>
2023-10-12 14:57:54 -0400

    get submitter name from first and last name

--- src/main/java/org/kpmp/dmd/DluPackageInventory.java
+++ src/main/java/org/kpmp/dmd/DluPackageInventory.java
@@ -27,7 +27,7 @@ public class DluPackageInventory {
     public DluPackageInventory(Package myPackage) {
         setDluPackageId(myPackage.getPackageId());
         setDluCreated(myPackage.getCreatedAt());
-        setDluSubmitter(myPackage.getSubmitter().getDisplayName());
+        setDluSubmitter(myPackage.getSubmitter().getFirstName() + " " + myPackage.getSubmitter().getLastName());
         setDluTis(myPackage.getTisName());
         setDluPackageType(myPackage.getPackageType());
         setDluSubjectId(myPackage.getSubjectId());
