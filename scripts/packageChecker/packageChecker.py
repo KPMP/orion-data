a47e5ae9e242486f21c9ee9689339cd21428397a
dert1129 <nathancreger1111@gmail.com>
2024-01-24 10:49:33 -0500

    test length of extra files list

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -69,6 +69,7 @@ class PackageChecker:
                         missing_files_list = ', '.join(missing_files_list)
                         extra_files_list = set(actual_file_names).difference(set(expected_file_names))
                         extra_files_list = ", ".join(extra_files_list)
+                        print(len(extra_files_list))
                         # if len(missing_files_list) != 0:
                         #     data = [
                         #         [package_id, missing_files_list]
