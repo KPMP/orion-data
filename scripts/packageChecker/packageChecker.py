d50ff6321464cac7ea7ebaca122cb8a1ce56b221
dert1129 <nathancreger1111@gmail.com>
2024-01-24 10:48:47 -0500

    test extra files writer

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -32,14 +32,14 @@ class PackageChecker:
     def find_empty_packages(self):
         empty_package_list = []
         missing_package_list = []
-        extra_package_list = []
-        # data = {}
-        missing_files_header = ["Package ID", "Missing Files"]
-        missing_files_csv = open("missing_files.csv", "w")
+        
+        # missing_files_header = ["Package ID", "Missing Files"]
+        # missing_files_csv = open("missing_files.csv", "w")
+        # missing_writer = csv.writer(missing_files_csv)
+        # missing_writer.writerow(missing_files_header)
+        
         extra_files_header = ['Package ID', 'Extra Files']
         extra_files_csv = open("extra_files.csv", "w")
-        missing_writer = csv.writer(missing_files_csv)
-        missing_writer.writerow(missing_files_header)
         extra_writer = csv.writer(extra_files_csv)
         extra_writer.writerow(extra_files_header)
         packages = self.dataLake.packages.find({})
@@ -69,23 +69,21 @@ class PackageChecker:
                         missing_files_list = ', '.join(missing_files_list)
                         extra_files_list = set(actual_file_names).difference(set(expected_file_names))
                         extra_files_list = ", ".join(extra_files_list)
-                        if len(missing_files_list) != 0:
-                            data = [
-                                [package_id, missing_files_list]
-                            ]
-                            missing_writer.writerows(data)
-                        elif len(extra_files_list != 0):
+                        # if len(missing_files_list) != 0:
+                        #     data = [
+                        #         [package_id, missing_files_list]
+                        #     ]
+                        #     missing_writer.writerows(data)
+                        if len(extra_files_list != 0):
                             data = [
                                 [package_id, extra_files_list]
                             ]
-                            print(data)
-                            extra_writer.writerow("test-package-id", "test-file")
                             extra_writer.writerows(data)
                     except:
                         missing_package_list.append(package_id)
                         
                         
-        missing_files_csv.close()
+        # missing_files_csv.close()
         extra_files_csv.close()
             
         # if len(empty_package_list) > 0:
