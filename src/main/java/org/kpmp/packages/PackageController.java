11b70cd71d566f88a675e1efe49b9a43f37abfb9
Zach Wright <zwright@umich.edu>
2024-07-08 15:29:33 -0400

    IP-463: Get for testing

--- src/main/java/org/kpmp/packages/PackageController.java
+++ src/main/java/org/kpmp/packages/PackageController.java
@@ -177,7 +177,7 @@ public class PackageController {
 		return true;
 	}
 
-	@RequestMapping(value = "/v1/packages/{packageId}/files/delete/{fileId}", method = RequestMethod.POST)
+	@RequestMapping(value = "/v1/packages/{packageId}/files/delete/{fileId}", method = RequestMethod.GET)
 	public @ResponseBody boolean deletePackage(@PathVariable("packageId") String packageId, @PathVariable("fileId") String fileId, HttpServletRequest request) {
 		HttpSession session = request.getSession(false);
 		String shibId = "";
