0b702f39cb49b91999fa761fd72d0aa677e7d294
Zach Wright <zwright@umich.edu>
2024-07-30 11:18:35 -0400

    IP-479: Throw that error again

--- src/main/java/org/miktmc/packages/PackageFileHandler.java
+++ src/main/java/org/miktmc/packages/PackageFileHandler.java
@@ -38,6 +38,7 @@ public class PackageFileHandler {
 		File fileToSave = new File(filePath);
 		if (!shouldAppend && fileToSave.exists()) {
 			logger.logErrorMessage(PackageFileHandler.class, packageId, "File " + filePath + " already exists.");
+			throw new FileAlreadyExistsException(fileToSave.getPath());
 		} else {
 			InputStream inputStream = file.getInputStream();
 			FileOutputStream fileOutputStream = new FileOutputStream(fileToSave, shouldAppend);
