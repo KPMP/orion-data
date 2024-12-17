607bc8e393990b27d44046dc3526de80326cec6b
Zach Wright <zwright@umich.edu>
2024-12-17 16:03:57 -0500

    KPMP-5665: uncomment slack stuff

--- scripts/packageChecker/packageChecker.py
+++ scripts/packageChecker/packageChecker.py
@@ -119,22 +119,22 @@ class PackageChecker:
         extra_files_csv.close()
         if len(empty_package_list) > 0:
             message = "Missing files in packages: " + ', '.join(empty_package_list)
-            # requests.post(
-            #     slack_url,
-            #     headers={'Content-type': 'application/json', },
-            #     data='{"text":"' + message + '"}')
+            requests.post(
+                slack_url,
+                headers={'Content-type': 'application/json', },
+                data='{"text":"' + message + '"}')
         if len(missing_package_list) > 0:
             message = "Missing package directories for packages: " + ', '.join(missing_package_list)
-            # requests.post(
-            #     slack_url,
-            #     headers={'Content-type': 'application/json', },
-            #     data='{"text":"' + message + '"}')
+            requests.post(
+                slack_url,
+                headers={'Content-type': 'application/json', },
+                data='{"text":"' + message + '"}')
         if len(extra_package_list) > 0:
             message = "Extra files for packages: " + ', '.join(extra_package_list)
-            # requests.post(
-            #     slack_url,
-            #     headers={'Content-type': 'application/json', },
-            #     data='{"text":"' + message + '"}')
+            requests.post(
+                slack_url,
+                headers={'Content-type': 'application/json', },
+                data='{"text":"' + message + '"}')
 
     def move_file_to_derived(self, package_directory, file_name):
         derived_dir = package_directory + "/derived"
